473ea19ca5083d52b28a92db08a02c25e4f9b9f5
mithunMavila <mithunm712@gmail.com>
2023-02-08 10:44:04 +0530

    last commit

---
+++ netflix.js
@@ -0,0 +1,58 @@
+
+function answer1(){
+    var ans = document.getElementById('ans1')
+    if(ans.style.display !== 'none'){
+        ans.style.display = 'none';
+    }else{
+        ans.style.display ='block';
+    }
+
+    
+}
+
+
+function answer2(){
+    var ans = document.getElementById('ans2')
+    if(ans.style.display !== 'none'){
+        ans.style.display = 'none';
+    }else{
+        ans.style.display ='block';
+    }
+}
+
+
+function answer3(){
+    var ans = document.getElementById('ans3')
+    if(ans.style.display !== 'none'){
+        ans.style.display = 'none';
+    }else{
+        ans.style.display ='block';
+    }
+}
+
+function answer4(){
+    var ans = document.getElementById('ans4')
+    if(ans.style.display !== 'none'){
+        ans.style.display = 'none';
+    }else{
+        ans.style.display ='block';
+    }
+}
+
+function answer5(){
+    var ans = document.getElementById('ans5')
+    if(ans.style.display !== 'none'){
+        ans.style.display = 'none';
+    }else{
+        ans.style.display ='block';
+    }
+}
+
+function answer6(){
+    var ans = document.getElementById('ans6')
+    if(ans.style.display !== 'none'){
+        ans.style.display = 'none';
+    }else{
+        ans.style.display ='block';
+    }
+}
